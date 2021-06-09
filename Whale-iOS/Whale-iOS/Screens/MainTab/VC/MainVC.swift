6a5bf448c014f1cc0c78b765d3bb42a45124a01a
Danna Lee <daeunlee0720@gmail.com>
2021-06-09 18:48:04 +0900

    [feat] 메인뷰 컴포넌트 추가

--- Whale-iOS/Whale-iOS/Screens/MainTab/VC/MainVC.swift
+++ Whale-iOS/Whale-iOS/Screens/MainTab/VC/MainVC.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class MainVC: UIViewController {
+    
+    //MARK: - Custom Variables
+    
+    
+    //MARK: - IBOutlets
 
+    
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +23,14 @@ class MainVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
+}
+
+extension MainVC {
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
-
+    
 }
