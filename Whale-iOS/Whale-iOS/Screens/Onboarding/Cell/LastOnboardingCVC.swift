f9a866965d4d5ad5eadb30ceae14dcc33d569eb4
hwangji-dev <melon2228@naver.com>
2021-06-11 01:35:27 +0900

    [Feat] Onboarding Process draft

---
+++ Whale-iOS/Whale-iOS/Screens/Onboarding/Cell/LastOnboardingCVC.swift
@@ -0,0 +1,42 @@
+//
+//  LastOnboardingCVC.swift
+//  Whale-iOS
+//
+//  Created by 황지은 on 2021/06/11.
+//
+
+import UIKit
+
+class LastOnboardingCVC: UICollectionViewCell {
+    
+    @IBOutlet var firstLabel: UILabel!
+    @IBOutlet var secondLabel: UILabel!
+    
+    override func awakeFromNib() {
+        customLabels()
+    }
+    
+    //MARK: - 라벨 AttributeFont 설정
+    func customLabels() {
+        /// set firstBigLabel fonts
+        let firstAttributedString = NSMutableAttributedString(string: firstLabel.text!, attributes: [
+                                                            .font: UIFont(name: "NotoSansCJKkr-Bold", size: 23)!,
+                                                            .foregroundColor: UIColor.brown_2,
+                                                            .kern: -1.15 ])
+        firstAttributedString.addAttribute(.font, value: UIFont(name: "NotoSansCJKkr-Regular", size: 23)!, range: (firstLabel.text! as NSString).range(of: "와 함께"))
+        firstAttributedString.addAttribute(.foregroundColor, value: UIColor.brown_2, range: (firstLabel.text! as NSString).range(of: "와 함께"))
+        
+        firstLabel.attributedText = firstAttributedString
+        firstLabel.sizeToFit()
+        
+        /// set secondBigLabel fonts
+        let secondAttributedString = NSMutableAttributedString(string: secondLabel.text!, attributes: [
+                                                            .font: UIFont(name: "NotoSansCJKkr-Bold", size: 23)!,
+                                                            .foregroundColor: UIColor.brown_2,
+                                                            .kern: -1.15 ])
+        secondAttributedString.addAttribute(.font, value: UIFont(name: "NotoSansCJKkr-Regular", size: 23)!, range: (secondLabel.text! as NSString).range(of: "매일"))
+        secondAttributedString.addAttribute(.foregroundColor, value: UIColor.brown_2, range: (secondLabel.text! as NSString).range(of: "매일"))
+        secondLabel.attributedText = secondAttributedString
+        secondLabel.sizeToFit()
+    }
+}
--- Whale-iOS/Whale-iOS/Screens/Onboarding/VC/OnboardingVC.swift
+++ Whale-iOS/Whale-iOS/Screens/Onboarding/VC/OnboardingVC.swift
@@ -9,43 +9,51 @@ import UIKit
 
 class OnboardingVC: UIViewController {
     
-
+    
     @IBOutlet var onboardingCV: UICollectionView!
     @IBOutlet var onboardingPageControl: UIPageControl!
+    @IBOutlet var nextBtn: UIButton!
+    var onboardingArray:[Onboarding] = []
     // 페이징 관련 index 정의 변수
     private var indexOfCellBeforeDragging = 0
     // ScreenSize 가져오는 변수
     let screenWidth = UIScreen.main.bounds.size.width
     let screenHeight = UIScreen.main.bounds.size.height
-    var onboardingArray:[Onboarding] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         onboardingCV.delegate = self
         onboardingCV.dataSource = self
         setOnboardingData()
-        // Do any additional setup after loading the view.
+        setNextBtnProperty()
     }
     
+    // Onboarding Data 구성
     func setOnboardingData() {
         onboardingArray.append(contentsOf:
                                 [Onboarding(firstPraiseText: "하루에 한 번씩", firstRange1Text: "", secondPraiseText: "칭찬해요", secondRange1Text: "해요", subExpText: "매일 매일 다른 칭찬 미션을 \n하나씩 보내드릴게요!", descriptionText: "원하는 시간에 리마인드 알림도 받을 수 있어요:)", subImgName: "onboarding_1_img_whale"),
                                  Onboarding(firstPraiseText: "나만의 고래를", firstRange1Text: "를", secondPraiseText: "춤 추게 해요", secondRange1Text: "해요", subExpText: "칭찬을 할 때 마다 \n나의 고래가 춤을 춰요!", descriptionText: "칭찬 횟수에 따라 고래의 레벨이 높아져요:)", subImgName: "onboarding_2_img_whale"),
                                  Onboarding(firstPraiseText: "칭찬을 모아보고", firstRange1Text: "칭찬을", secondPraiseText: "칭찬 랭킹을 확인해요", secondRange1Text: "칭찬 랭킹", subExpText: "내가 한 칭찬을 모아보고 \n칭찬 대상에 대한 랭킹도 확인해요!", descriptionText: "누구에게 언제 어떤 칭찬을 했는지 한 번에 확인해요:)", subImgName: "onboarding_3_img_whale")
                                 ])
-        onboardingPageControl.numberOfPages = onboardingArray.count
+        onboardingPageControl.numberOfPages = onboardingArray.count + 1
     }
     
-    // 오른쪽으로 넘길 때
+    // 하단 버튼 초기 상태 set
+    func setNextBtnProperty() {
+        nextBtn.setTitle("다음", for: .normal)
+        nextBtn.layer.cornerRadius = 25
+    }
+    
+    // Left Paging (오른쪽으로 넘길 때)
     private func indexOfMajorCell() -> Int {
         let itemWidth = screenWidth
         let proportionalOffset = (onboardingCV.contentOffset.x / itemWidth)+0.3
         let index = Int(round(proportionalOffset))
-        let safeIndex = max(0, min(onboardingArray.count - 1, index))
+        let safeIndex = max(0, min(onboardingArray.count, index))
         return safeIndex
     }
     
-    // 왼쪽으로 넘길 때
+    // Right Paging (왼쪽으로 넘길 때)
     private func indexOfBeforCell() -> Int {
         let itemWidth = screenWidth
         let proportionalOffset = (onboardingCV.contentOffset.x / itemWidth)
@@ -54,28 +62,50 @@ class OnboardingVC: UIViewController {
         return safeIndex
     }
     
-    
-//    @IBAction func pageChanged(_ sender: UIPageControl) {
-//        let pc = sender
-//
-//        onboardingCV.scrollToItem(at: IndexPath(item: pc.currentPage, section: 0), at: .centeredHorizontally, animated: true)
-//    }
-}
+    // TouchUpNextBtn (nextBtn 눌렀을 때 액션)
+    @IBAction func touchUpToNextPageBtn(_ sender: Any) {
+        let indexPath = IndexPath(row: onboardingPageControl.currentPage + 1, section: 0)
+        
+        if indexPath.row == 4 {
+            // 회원가입 뷰로 이동
 
+            let signUpSB: UIStoryboard = UIStoryboard(name: "SignUp", bundle: nil)
+            if let signUpVC = signUpSB.instantiateViewController(identifier: "SignUpVC") as? SignUpVC {
+                self.navigationController?.pushViewController(signUpVC, animated: true)
+            }
+        }
+        else {
+            // 페이지 이동
+            onboardingCV.collectionViewLayout.collectionView!.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        }
+    }
+}
+//MARK: - CollectionView Protocol
 extension OnboardingVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return onboardingArray.count
+        
+        return onboardingArray.count + 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let firstOnboardingCell = collectionView.dequeueReusableCell(withReuseIdentifier: "OnboardingCVCell", for: indexPath) as! OnboardingCVCell
         
-        firstOnboardingCell.customLabels(onboardingArray[indexPath.row].firstPraiseText, onboardingArray[indexPath.row].secondPraiseText, onboardingArray[indexPath.row].firstRange1Text, onboardingArray[indexPath.row].secondRange1Text, onboardingArray[indexPath.row].subExpText, descriptionText: onboardingArray[indexPath.row].descriptionText)
-        firstOnboardingCell.whaleImageView.image = UIImage(named: onboardingArray[indexPath.row].subImgName)
-        
-        return firstOnboardingCell
-        
+        if indexPath.row < 3 {
+            let firstOnboardingCell = collectionView.dequeueReusableCell(withReuseIdentifier: "OnboardingCVCell", for: indexPath) as! OnboardingCVCell
+            
+            firstOnboardingCell.customLabels(onboardingArray[indexPath.row].firstPraiseText, onboardingArray[indexPath.row].secondPraiseText, onboardingArray[indexPath.row].firstRange1Text, onboardingArray[indexPath.row].secondRange1Text, onboardingArray[indexPath.row].subExpText, descriptionText: onboardingArray[indexPath.row].descriptionText)
+            firstOnboardingCell.whaleImageView.image = UIImage(named: onboardingArray[indexPath.row].subImgName)
+            
+            return firstOnboardingCell
+        }
+        else if indexPath.row == 3 {
+            let lastOnboardingCell = collectionView.dequeueReusableCell(withReuseIdentifier: "LastOnboardingCVC", for: indexPath) as! LastOnboardingCVC
+            
+            return lastOnboardingCell
+        }
+        else {
+            return UICollectionViewCell()
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -90,7 +120,7 @@ extension OnboardingVC: UICollectionViewDelegate, UICollectionViewDataSource, UI
 
 //MARK: - collectionView Horizontal Scrolling Magnetic Effect 적용
 extension OnboardingVC : UIScrollViewDelegate {
- 
+    
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
         indexOfCellBeforeDragging = indexOfMajorCell()
     }
@@ -100,14 +130,6 @@ extension OnboardingVC : UIScrollViewDelegate {
         if velocity.x > 0 {
             let indexOfMajorCell = self.indexOfMajorCell()
             let indexPath = IndexPath(row: indexOfMajorCell, section: 0)
-            if indexPath.item == 4 {
-                onboardingPageControl.isHidden = true
-                UIView.animate(withDuration: 1.0, animations:
-                                {
-                                   // self.startBtn.alpha = 1.0
-                                    self.view.layoutIfNeeded()
-                                })
-            }
             onboardingCV.collectionViewLayout.collectionView!.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
         }else{
             let indexOfBeforCell = self.indexOfBeforCell()
@@ -117,9 +139,19 @@ extension OnboardingVC : UIScrollViewDelegate {
         }
     }
     
+    
     //MARK: - scroll animation이 끝나고 적용되는 함수
     func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        let indexOfMajorCell = self.indexOfMajorCell()
+        let indexPath = IndexPath(row: indexOfMajorCell, section: 0)
         // scrollAnimation이 끝나고 pageControl의 현재 페이지를 animation이 끝난 상태값으로 바꿔준다.
         onboardingPageControl.currentPage = Int(round(onboardingCV.contentOffset.x / onboardingCV.frame.size.width))
+        
+        if indexPath.row == 3 {
+            nextBtn.setTitle("시작하기", for: .normal)
+        }
+        else {
+            nextBtn.setTitle("다음", for: .normal)
+        }
     }
 }
