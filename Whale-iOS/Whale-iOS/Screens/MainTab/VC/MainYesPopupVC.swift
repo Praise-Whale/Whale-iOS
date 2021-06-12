a613b93925fcd7798abc326da2cafad191c76371
Danna Lee <daeunlee0720@gmail.com>
2021-06-12 19:25:25 +0900

    [feat] 텍스트필드 clear 버튼 구현

--- Whale-iOS/Whale-iOS/Screens/MainTab/VC/MainYesPopupVC.swift
+++ Whale-iOS/Whale-iOS/Screens/MainTab/VC/MainYesPopupVC.swift
@@ -37,6 +37,7 @@ class MainYesPopupVC: UIViewController {
         super.viewDidLoad()
         
         setDefaultStyle()
+        setNameTextNotExists()
 
     }
     
@@ -60,11 +61,14 @@ extension MainYesPopupVC {
         titleUnderView.makeRounded(cornerRadius: 1)
         
         nameTextField.addLeftPadding(width: 13)
-        nameTextField.addRightPadding(width: 13)
+//        nameTextField.addRightPadding(width: 5)
         nameTextField.font = .NotoSansRegular(size: 13)
         nameTextField.layer.borderColor = UIColor.yellow_3.cgColor
         nameTextField.layer.borderWidth = 1
         nameTextField.placeholder = "이름을 실명으로 입력해주세요."
+        nameTextField.makeRounded(cornerRadius: 12)
+        nameTextField.clearButtonMode = .always
+        nameTextField.autocorrectionType = .no
         
         recentTitleLabel.textColor = .grey_2
         recentTitleLabel.font = .NotoSansRegular(size: 14)
@@ -77,10 +81,10 @@ extension MainYesPopupVC {
     }
     
     func setNameTextExists() {
-        
+        submitBtn.backgroundColor = .sand_yellow
     }
     
     func setNameTextNotExists() {
-        
+        submitBtn.backgroundColor = .grey_1
     }
 }
