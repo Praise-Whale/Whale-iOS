8905ee2f0e87d39f284d36dc1f23e43d6a41d1ca
hwangJi-dev <melon2228@swu.ac.kr>
2021-07-24 03:13:40 +0900

    [chore] OnbordingCVCell(1~3단계 온보딩) 오토레이아웃을 위한 수정

--- Whale-iOS/Whale-iOS/Screens/Onboarding/Cell/OnboardingCVCell.swift
+++ Whale-iOS/Whale-iOS/Screens/Onboarding/Cell/OnboardingCVCell.swift
@@ -13,18 +13,19 @@ class OnboardingCVCell: UICollectionViewCell {
     @IBOutlet var firstPraiseLabel: CustomLabel!
     @IBOutlet var secondPraiseLabel: UILabel!
     @IBOutlet var subExplanationLabel: UILabel!
+    @IBOutlet var sub2ExplanationLabel: UILabel!
     @IBOutlet var descLabel: CustomLabel!
     @IBOutlet var whaleImageView: UIImageView!
     
     
     //MARK: - 라벨 AttributeFont 설정
-    func customLabels(_ firstPraiseText: String, _ secondPraiseText: String, _ firstRegularRange1: String, _ secondRegularRange1: String, _ subExplainText: String, descriptionText: String) {
+    func customLabels(_ firstPraiseText: String, _ secondPraiseText: String, _ firstRegularRange1: String, _ secondRegularRange1: String, _ subExplainText: String, _ sub2ExplainText: String, descriptionText: String) {
         /// set firstBigLabel fonts
         let firstPraiseAttributedString = NSMutableAttributedString(string: firstPraiseText, attributes: [
-                                                            .font: UIFont(name: "NotoSansCJKkr-Bold", size: 26)!,
+                                                            .font: UIFont(name: "AppleSDGothicNeoB00", size: 26)!,
                                                             .foregroundColor: UIColor.brown_2,
                                                             .kern: -1.3 ])
-        firstPraiseAttributedString.addAttribute(.font, value: UIFont(name: "NotoSansCJKkr-Regular", size: 26)!, range: (firstPraiseText as NSString).range(of: firstRegularRange1))
+        firstPraiseAttributedString.addAttribute(.font, value: UIFont(name: "AppleSDGothicNeoR00", size: 26)!, range: (firstPraiseText as NSString).range(of: firstRegularRange1))
         firstPraiseAttributedString.addAttribute(.foregroundColor, value: UIColor.brown_2, range: (firstPraiseText as NSString).range(of: firstRegularRange1))
         firstPraiseLabel.attributedText = firstPraiseAttributedString
         
@@ -33,20 +34,27 @@ class OnboardingCVCell: UICollectionViewCell {
         
         /// set secondBigLabel fonts
         let secondPraiseAttributedString = NSMutableAttributedString(string: secondPraiseText, attributes: [
-                                                            .font: UIFont(name: "NotoSansCJKkr-Bold", size: 26)!,
+                                                            .font: UIFont(name: "AppleSDGothicNeoB00", size: 26)!,
                                                             .foregroundColor: UIColor.brown_2,
                                                             .kern: -1.3 ])
-        secondPraiseAttributedString.addAttribute(.font, value: UIFont(name: "NotoSansCJKkr-Regular", size: 26)!, range: (secondPraiseText as NSString).range(of: secondRegularRange1))
+        secondPraiseAttributedString.addAttribute(.font, value: UIFont(name: "AppleSDGothicNeoR00", size: 26)!, range: (secondPraiseText as NSString).range(of: secondRegularRange1))
         secondPraiseAttributedString.addAttribute(.foregroundColor, value: UIColor.brown_2, range: (secondPraiseText as NSString).range(of: secondRegularRange1))
         secondPraiseLabel.attributedText = secondPraiseAttributedString
         
         /// set subExplanationLabel fonts
         let subExpAttributedString = NSMutableAttributedString(string: subExplainText, attributes: [
-                                                            .font: UIFont(name: "NotoSansCJKkr-Regular", size: 16)!,
+                                                            .font: UIFont(name: "AppleSDGothicNeoR00", size: 16)!,
                                                             .foregroundColor: UIColor.brown_2,
                                                                 .kern: -0.8])
         subExplanationLabel.attributedText = subExpAttributedString
         subExplanationLabel.sizeToFit()
+        
+        let sub2ExpAttributedString = NSMutableAttributedString(string: sub2ExplainText, attributes: [
+                                                            .font: UIFont(name: "AppleSDGothicNeoR00", size: 16)!,
+                                                            .foregroundColor: UIColor.brown_2,
+                                                                .kern: -0.8])
+        sub2ExplanationLabel.attributedText = sub2ExpAttributedString
+        sub2ExplanationLabel.sizeToFit()
         descLabel.text = descriptionText
         descLabel.sizeToFit()
     }
