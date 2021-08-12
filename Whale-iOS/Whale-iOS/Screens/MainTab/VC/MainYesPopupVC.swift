425dcd99ddc9e3dc7347525c0291c59b0d252936
Danna <daeunlee0720@gmail.com>
2021-08-13 06:23:23 +0900

    [feat] 텍스트필드 관련 모든 로직 구현

--- Whale-iOS/Whale-iOS/Screens/MainTab/VC/MainYesPopupVC.swift
+++ Whale-iOS/Whale-iOS/Screens/MainTab/VC/MainYesPopupVC.swift
@@ -11,6 +11,8 @@ class MainYesPopupVC: UIViewController {
     
     //MARK: - Custom Variables
     
+    var wordCount: Int = 0
+    
     
     //MARK: - IBOutlets
     
@@ -33,21 +35,45 @@ class MainYesPopupVC: UIViewController {
     @IBOutlet var submitBtn: UIButton!
     
     //MARK: - Lifecycle Methods
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        nameTextField.delegate = self
+        
+        keyBoardAction()
         setDefaultStyle()
-        setNameTextNotExists()
-
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        nameTextField.becomeFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
     
     //MARK: - IBActions
-
+    
     @IBAction func closeBtnDidTap(_ sender: Any) {
         self.dismiss(animated: false, completion: nil)
     }
     
+    @IBAction func nameTextFieldEditChanged(_ sender: Any) {
+        
+        checkMaxLength(textField: nameTextField, maxLength: 6)
+        
+        if nameTextField.text?.count == 0 {
+            wordCount = 0
+            setNameTextNotExists()
+        }
+        else {
+            wordCount = nameTextField.text!.count
+            setNameTextExists()
+        }
+    }
     
 }
 
@@ -66,7 +92,6 @@ extension MainYesPopupVC {
         titleUnderView.backgroundColor = .sand_yellow
         titleUnderView.makeRounded(cornerRadius: 1)
         
-        nameView.layer.borderColor = UIColor.yellow_3.cgColor
         nameView.layer.borderWidth = 1
         nameView.backgroundColor = .white
         nameView.makeRounded(cornerRadius: 12)
@@ -76,10 +101,16 @@ extension MainYesPopupVC {
         nameTextField.clearButtonMode = .always
         nameTextField.autocorrectionType = .no
         
+        countLabel.font = .AppleSDGothicR(size: 13)
+        countLabel.text = "\(wordCount)/6"
+        countLabel.textColor = .grey_2
+        
         recentTitleLabel.textColor = .grey_2
         recentTitleLabel.font = .AppleSDGothicR(size: 14)
         recentTitleLabel.text = "최근 칭찬한 사람"
         recentTitleLabel.letterSpacing = -0.7
+        
+        recentCollectionView.backgroundColor = .white
         
         submitBtn.makeRounded(cornerRadius: submitBtn.frame.height/2)
         submitBtn.setTitle("확인", for: .normal)
@@ -87,12 +118,52 @@ extension MainYesPopupVC {
     }
     
     func setNameTextExists() {
+        countLabel.text = "\(wordCount)/6"
+        
+        if let text = countLabel.text {
+            let attributedStr = NSMutableAttributedString(string: countLabel.text ?? "")
+            attributedStr.addAttribute(NSAttributedString.Key(rawValue: kCTFontAttributeName as String),
+                                       value: UIFont(name: "AppleSDGothicNeoR00", size: 13.0)!, range: (text as NSString).range(of: "\(wordCount)"))
+            attributedStr.addAttribute(.foregroundColor, value: UIColor.brown_1, range: (countLabel.text! as NSString).range(of: "\(wordCount)"))
+            countLabel.attributedText = attributedStr
+        }
+        
         submitBtn.backgroundColor = .sand_yellow
     }
     
     func setNameTextNotExists() {
+        countLabel.text = "\(wordCount)/6"
+        countLabel.textColor = .grey_2
+        
         submitBtn.backgroundColor = .grey_1
+    }
+    
+    func keyBoardAction() {
+        /// 키보드 디텍션
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustInputView), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustInputView), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func adjustInputView(noti: Notification) {
+        /// 키보드 활성화/비활성화 분기처리
+        if noti.name == UIResponder.keyboardWillShowNotification {
+            nameView.layer.borderColor = UIColor.yellow_4.cgColor
+            
+            countLabel.isHidden = false
+        } else {
+            nameView.layer.borderColor = UIColor.grey_1.cgColor
+            
+            countLabel.isHidden = true
+        }
     }
 }
 
+//MARK: - UITextFieldDelegate
 
+extension MainYesPopupVC: UITextFieldDelegate {
+    func checkMaxLength(textField: UITextField!, maxLength: Int) {
+        if(textField.text?.count ?? 0 > maxLength) {
+            textField.deleteBackward()
+        }
+    }
+}
