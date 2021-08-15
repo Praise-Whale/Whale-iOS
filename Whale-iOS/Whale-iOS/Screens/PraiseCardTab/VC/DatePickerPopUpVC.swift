cef1028a4a34f9f1b3e0d214b9468d9207b891b5
hwangJi-dev <melon2228@swu.ac.kr>
2021-08-16 03:05:31 +0900

    [Feat] customDatePicker 팝업뷰 구현완료

--- Whale-iOS/Whale-iOS/Screens/PraiseCardTab/VC/DatePickerPopUpVC.swift
+++ Whale-iOS/Whale-iOS/Screens/PraiseCardTab/VC/DatePickerPopUpVC.swift
@@ -8,13 +8,19 @@
 import UIKit
 import SnapKit
 
+protocol selectYearMonthFromPicker {
+    func setYearMonth(_ year: Int,_ month: Int)
+}
+
 class DatePickerPopUpVC: UIViewController {
 
     var datePopUpView: praiseDatePickerView = praiseDatePickerView()
+    var delegate: selectYearMonthFromPicker?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setPopUpViewLayout()
+        notificationAddObserver()
     }
     
     func setPopUpViewLayout() {
@@ -25,5 +31,29 @@ class DatePickerPopUpVC: UIViewController {
             make.centerX.equalTo(self.view)
             make.centerY.equalTo(self.view)
         }
+    }
+}
+
+//MARK: - Notifiication
+extension DatePickerPopUpVC {
+    func notificationAddObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(dismissDatePickerPopUp), name: .dismissDatePickerPopUp, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(selectDatePickerPopUp), name: .selectDatePickerPopUp, object: nil)
+    }
+    
+    @objc func dismissDatePickerPopUp() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func selectDatePickerPopUp(notification: Notification) {
+        print("come")
+        let dict = notification.object as! NSDictionary
+        let year = dict["year"] as! Int
+        let month = dict["month"] as! Int
+        print("comeyear", year)
+        print("comemonth", month)
+        
+        self.delegate?.setYearMonth(year, month)
+        self.dismiss(animated: true, completion: nil)
     }
 }
--- Whale-iOS/Whale-iOS/Screens/PraiseCardTab/VC/praiseDatePickerView.swift
+++ Whale-iOS/Whale-iOS/Screens/PraiseCardTab/VC/praiseDatePickerView.swift
@@ -11,14 +11,22 @@ import SnapKit
 class praiseDatePickerView: UIView {
     
     var yearArray:[String] = []
-    var monthArray:[String] = ["1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월","전체"]
+    var monthArray:[String] = ["전체", "1월","2월","3월","4월","5월","6월","7월","8월","9월","10월","11월","12월"]
     let loopingMargin: Int = 40
     let date = Date()
     let calendar = Calendar.current
+    var year: Int = 0
+    var month: Int = 0
     var currentYearRow: Int = 0
     var currentMonthRow: Int = 0
+    var firstMonthRow: Int = 0
     var selectedYearTitle: Int = 0
     var selectedMonthTitle: Int = 0
+    var firstPraiseDate = UserDefaults.standard.string(forKey: "praiseFirstDate")
+    var firstPraiseYearRow: Int = 0
+    var firstPraiseMonthRow: Int = 0
+    var firstPraiseYear: Int = 0
+    var firstPraiseMonth: Int = 0
     
     @IBOutlet var praiseDateLabel: UILabel!
     @IBOutlet var customDatePickerView: CustomPickerView!
@@ -26,14 +34,14 @@ class praiseDatePickerView: UIView {
     @IBOutlet var settingBtn: UIButton!
     
     override func draw(_ rect: CGRect) {
-       // setUIComponentProperty()
-        setCurrentDateRow()
+        setDefaultDateRow()
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         xibSetup()
         makeYearArray()
+        setFirstPraiseYearMonth()
     }
     
     override func prepareForInterfaceBuilder() {
@@ -51,6 +59,7 @@ class praiseDatePickerView: UIView {
         }
         view.frame = bounds
         view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.layer.cornerRadius = 16
         addSubview(view)
         setUIComponentProperty()
     }
@@ -60,19 +69,25 @@ class praiseDatePickerView: UIView {
         let nib = UINib(nibName: nib, bundle: bundle)
         return nib.instantiate(withOwner: self, options: nil).first as? UIView
     }
+}
+
+//MARK: - UI 레이아웃 구성부
+extension praiseDatePickerView {
     
-    //MARK: - setUIComponentProperty
+    // setUIComponentProperty
     func setUIComponentProperty() {
+        //praiseDateLabel
         praiseDateLabel.font = .AppleSDGothicR(size: 20)
         praiseDateLabel.text = "칭찬 날짜"
         praiseDateLabel.textColor = .brown_2
         
+        //settingBtn
         settingBtn.layer.cornerRadius = 20
         settingBtn.tintColor = .black
         settingBtn.titleLabel?.letterSpacing = -0.75
     }
     
-    //MARK: - yearArray 만드는 함수
+    // yearArray 만드는 함수
     func makeYearArray() {
         DispatchQueue.main.async { [self] in
             for i in 2010...2050 {
@@ -81,27 +96,64 @@ class praiseDatePickerView: UIView {
         }
     }
     
-    //MARK: - 현재 year, month 기반으로 picker를 설정하는 함수
-    func setCurrentDateRow() {
+    // 유저가 첫 칭찬을 한 날짜 기반으로 firstPraiseYear, firstPraiseMonth에 값을 지정하는 함수
+    func setFirstPraiseYearMonth() {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let convertDate = dateFormatter.date(from: firstPraiseDate ?? "")
+        let yyFormatter = DateFormatter()
+        yyFormatter.dateFormat = "yyyy"
+        let convertYearStr = yyFormatter.string(from: convertDate ?? Date())
+        let mmFormatter = DateFormatter()
+        mmFormatter.dateFormat = "MM"
+        let convertMonthStr = mmFormatter.string(from: convertDate ?? Date())
+        firstPraiseYear = Int(convertYearStr) ?? 0
+        firstPraiseMonth = Int(convertMonthStr) ?? 0
+    }
+    
+    // 현재 year, 전체 month 기반으로 picker를 설정하는 함수
+    func setDefaultDateRow() {
         let components = calendar.dateComponents([.year, .month], from: date)
-        let year =  components.year ?? 0
-        let month = components.month ?? 0
         let loopingYearIndex = (loopingMargin / 2) * yearArray.count
         let loopingMonthIndex = (loopingMargin / 2) * monthArray.count
+        year =  components.year ?? 0
+        month = components.month ?? 0
+        
+        // picker를 선택안하고 설정버튼을 눌렀을 경우 default값이 currentyear가 되도록 설정
+        selectedYearTitle = year
         
         for i in 0...yearArray.count - 1 {
-            if yearArray[i] == String(year) + "년" {
+            if yearArray[i] == year.toString() + "년" {
                 currentYearRow = i
+            }
+            if yearArray[i] == firstPraiseYear.toString() + "년" {
+                firstPraiseYearRow = i
             }
         }
         
         for i in 0...monthArray.count - 1 {
-            if monthArray[i] == String(month) + "월" {
+            if monthArray[i] == month.toString() + "월" {
                 currentMonthRow = i
             }
+            if monthArray[i] == firstPraiseMonth.toString() + "월" {
+                firstPraiseMonthRow = i
+            }
         }
+        
         customDatePickerView.selectRow(loopingYearIndex + currentYearRow, inComponent: 0, animated: false)
-        customDatePickerView.selectRow(loopingMonthIndex + currentMonthRow, inComponent: 1, animated: false)
+        customDatePickerView.selectRow(loopingMonthIndex + firstMonthRow, inComponent: 1, animated: false)
+    }
+    
+    // dismiss when set date
+    @IBAction func setDateBtn(_ sender: UIButton) {
+        let yearMonthDict = [ "year": selectedYearTitle, "month": selectedMonthTitle]
+        NotificationCenter.default.post(name: Notification.Name.selectDatePickerPopUp, object: yearMonthDict)
+    }
+    
+    // dismiss datePicker
+    @IBAction func dismissBtn(_ sender: UIButton) {
+        NotificationCenter.default.post(name: Notification.Name.dismissDatePickerPopUp, object: nil)
     }
 }
 
@@ -128,7 +180,10 @@ extension praiseDatePickerView: UIPickerViewDelegate {
         let components = calendar.dateComponents([.year, .month], from: date)
         let year =  components.year ?? 0
         let month = components.month ?? 0
+        let loopingYearIndex = (loopingMargin / 2) * yearArray.count
+        let monthLoopingIndex = (loopingMargin / 2) * monthArray.count
         let selectedYearRow = customDatePickerView.selectedRow(inComponent: 0) % yearArray.count
+        let selectedmonthRow = customDatePickerView.selectedRow(inComponent: 1) % monthArray.count
         
         for i in 0...yearArray.count - 1 {
             if selectedYearRow == i {
@@ -136,28 +191,47 @@ extension praiseDatePickerView: UIPickerViewDelegate {
             }
         }
         
-        if component == 0 {
-            let loopingIndex = (loopingMargin / 2) * yearArray.count
-            
-            if (year < selectedYearTitle) {
-                customDatePickerView.selectRow(loopingIndex + currentYearRow, inComponent: 0, animated: true)
+        for i in 0...monthArray.count - 1 {
+            if selectedmonthRow == i {
+                selectedMonthTitle = i
             }
         }
-        else {
-            let loopingIndex = (loopingMargin / 2) * monthArray.count
-            let selectedRow = customDatePickerView.selectedRow(inComponent: component) % monthArray.count
-            
-            for i in 0...monthArray.count - 1 {
-                if selectedRow == i {
-                    selectedMonthTitle = i
-                }
+        
+        // 현재년도와 같지만 월을 앞서갔을 때
+        if (year == selectedYearTitle && month < selectedMonthTitle) {
+            customDatePickerView.selectRow(monthLoopingIndex + currentMonthRow, inComponent: 1, animated: true)
+            selectedMonthTitle = month
+        }
+        
+        // 현재년도 이후로 피커를 돌릴 때
+        else if (year < selectedYearTitle) {
+            // 현재년도와 월을 모두 앞서갔을 때
+            if (month < selectedMonthTitle) {
+                customDatePickerView.selectRow(monthLoopingIndex + currentMonthRow, inComponent: 1, animated: true)
+                selectedMonthTitle = month
             }
-            
-            // 현재년도와 같지만 월을 앞서갔을 때
-            if (year == selectedYearTitle && month < selectedMonthTitle + 1) {
-                customDatePickerView.selectRow(loopingIndex + currentMonthRow, inComponent: 1, animated: true)
+            customDatePickerView.selectRow(loopingYearIndex + currentYearRow, inComponent: 0, animated: true)
+            selectedYearTitle = year
+        }
+        
+        
+        // 첫 칭찬 시점 이전으로 월 피커를 돌릴 때 (년도는 같은 경우)
+        if (firstPraiseYear == selectedYearTitle && firstPraiseMonth > selectedMonthTitle) {
+            // '전체'인 경우 제외
+            if selectedMonthTitle != 0 {
+                customDatePickerView.selectRow(monthLoopingIndex + firstPraiseMonthRow, inComponent: 1, animated: true)
+                selectedMonthTitle = firstPraiseMonth
             }
         }
+        // 첫 칭찬 시점 이전으로 년도 피커를 돌릴 때 (선택된 월 상관없이 모두 firstDate로 이동)
+        else if (firstPraiseYear > selectedYearTitle) {
+            
+            customDatePickerView.selectRow(monthLoopingIndex + firstPraiseMonthRow, inComponent: 1, animated: true)
+            selectedMonthTitle = firstPraiseMonth
+            customDatePickerView.selectRow(loopingYearIndex + firstPraiseYearRow, inComponent: 0, animated: true)
+            selectedYearTitle = firstPraiseYear
+        }
+        
         pickerView.tintColor = .black
     }
 }
