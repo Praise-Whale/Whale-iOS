bd6358b483f19096ff91ca423dda380d5cc11f10
hwangJi-dev <melon2228@swu.ac.kr>
2021-08-16 23:49:12 +0900

    [Feat] 레벨 정보 디테일뷰 구현

---
+++ Whale-iOS/Whale-iOS/Screens/LevelTab/VC/LevelInfoVC.swift
@@ -0,0 +1,19 @@
+//
+//  LevelInfoVC.swift
+//  Whale-iOS
+//
+//  Created by 황지은 on 2021/08/16.
+//
+
+import UIKit
+
+class LevelInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func dismissBtn(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
