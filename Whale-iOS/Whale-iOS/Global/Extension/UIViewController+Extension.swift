3fe424c37ae1b83a1f8ecba13c17e0a98f2b24c9
Danna <daeunlee0720@gmail.com>
2021-09-30 23:29:59 +0900

    [fix] 토스트 익스텐션 height 매개변수 추가

--- Whale-iOS/Whale-iOS/Global/Extension/UIViewController+Extension.swift
+++ Whale-iOS/Whale-iOS/Global/Extension/UIViewController+Extension.swift
@@ -7,9 +7,9 @@
 
 import UIKit
 extension UIViewController{
-    func showToast(message: String) {
+    func showToast(message: String, bottom: CGFloat = 165) {
         let width_variable: CGFloat = 33
-        let toastLabel = UILabel(frame: CGRect(x: width_variable, y: self.view.frame.size.height-165, width: view.frame.size.width-2*width_variable, height: 44))
+        let toastLabel = UILabel(frame: CGRect(x: width_variable, y: self.view.frame.size.height-bottom, width: view.frame.size.width-2*width_variable, height: 44))
         // 뷰가 위치할 위치를 지정해준다. 여기서는 아래로부터 100만큼 떨어져있고, 너비는 양쪽에 10만큼 여백을 가지며, 높이는 35로
         toastLabel.backgroundColor = UIColor(red: 255/255, green: 212/255, blue: 100/255, alpha: 0.53)
         toastLabel.textColor = UIColor.brown_1
