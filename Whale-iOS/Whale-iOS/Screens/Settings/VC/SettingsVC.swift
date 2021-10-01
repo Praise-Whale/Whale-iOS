4e935fec63cbbb8c1fe5874bfdc9d67ec52bd800
Danna <daeunlee0720@gmail.com>
2021-10-01 19:13:46 +0900

    [feat] 메일 보내기

--- Whale-iOS/Whale-iOS/Screens/Settings/VC/SettingsVC.swift
+++ Whale-iOS/Whale-iOS/Screens/Settings/VC/SettingsVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MessageUI
 
 class SettingsVC: UIViewController {
 
@@ -35,6 +36,28 @@ class SettingsVC: UIViewController {
     @IBAction func changeAlertStateSwitch(_ sender: Any) {
         if changeAlertStateSwitch.isOn {
             self.showToast(message: "앞으로 오전 9:00 에 칭찬 알림을 보내드릴게요!", bottom: 115)
+        }
+    }
+    
+    @IBAction func mailDidTap(_ sender: Any) {
+        if MFMailComposeViewController.canSendMail() {
+            
+            let composeVC = MFMailComposeViewController()
+            composeVC.mailComposeDelegate = self
+
+            // Configure the fields of the interface.
+            composeVC.setToRecipients(["Co.Goraedan@gmail.com"])
+            composeVC.setSubject("[칭찬할고래 문의]")
+            composeVC.setMessageBody("1. 문의 유형 ( 문의, 버그 제보, 탈퇴하기, 기타) : \n2. 회원 닉네임 (필요시 기입) : \n3. 문의 내용 : \n\n\n문의하신 사항은 칭찬할고래팀이 신속하게 처리하겠습니다. 감사합니다 :)", isHTML: false)
+
+            // Present the view controller modally.
+            present(composeVC, animated: true) {
+                composeVC.showToast(message: "문의 내용을 적어 메일로 보내주세요!", bottom: 115)
+            }
+            
+        } else {
+            // Mail앱을 사용할 수 없을 경우
+            self.showToast(message: "메일 앱을 설치해주세요", bottom: 115)
         }
     }
     
@@ -76,5 +99,27 @@ extension SettingsVC {
         }
         
         settingDetailLabel[0].text = UserDefaults.standard.string(forKey: "nickName")
+    }
+}
+
+extension SettingsVC: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        switch result {
+        case .sent:
+            // 메일 발송 성공 ( 인터넷이 안되는 경우도 sent처리되고, 인터넷이 연결되면 메일이 발송됨. )
+            self.showToast(message: "소중한 의견 감사합니다! :)", bottom: 115)
+        case .saved:
+            // 메일 임시 저장
+            self.showToast(message: "메일이 임시 저장 되었어요", bottom: 115)
+        case .cancelled:
+            // 메일 작성 취소
+            self.showToast(message: "메일 작성이 취소 되었어요", bottom: 115)
+        case .failed:
+            // 메일 발송 실패 (오류 발생)
+            self.showToast(message: "메일 발송에 실패했어요 :(", bottom: 115)
+        @unknown default:
+            self.showToast(message: "메일 발송에 실패했어요 :(", bottom: 115)
+        }
+        dismiss(animated: true)
     }
 }
