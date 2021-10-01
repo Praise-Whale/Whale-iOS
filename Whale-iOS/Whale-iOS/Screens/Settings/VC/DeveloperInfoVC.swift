810d1310a6735530215aeb59d20ef72ebd62add7
Danna <daeunlee0720@gmail.com>
2021-10-01 18:41:32 +0900

    [feat] 개발자 정보

---
+++ Whale-iOS/Whale-iOS/Screens/Settings/VC/DeveloperInfoVC.swift
@@ -0,0 +1,32 @@
+//
+//  DeveloperInfoVC.swift
+//  Whale-iOS
+//
+//  Created by Danna Lee on 2021/10/01.
+//
+
+import UIKit
+
+class DeveloperInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backBtnDidTap(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
