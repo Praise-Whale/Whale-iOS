f647acd54fd1153d4a5d39297307c4db72eede5a
Danna <daeunlee0720@gmail.com>
2021-10-01 17:54:54 +0900

    [feat] 컴포넌트 연결

---
+++ Whale-iOS/Whale-iOS/Screens/Settings/VC/ChangeNicknamePopupVC.swift
@@ -0,0 +1,40 @@
+//
+//  changeNicknamePopupVC.swift
+//  Whale-iOS
+//
+//  Created by Danna Lee on 2021/10/01.
+//
+
+import UIKit
+
+class ChangeNicknamePopupVC: UIViewController {
+    
+    //MARK: - Custom Variables
+    
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var titleUnderbarView: UIView!
+    
+    @IBOutlet var textfieldWrapView: UIView!
+    @IBOutlet var nicknameTextfield: UITextField!
+    @IBOutlet var wordCountLabel: UILabel!
+    
+    @IBOutlet var modifyBtn: UIButton!
+    
+    //MARK: - Lifecycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - IBActions
+
+    
+}
+
+extension ChangeNicknamePopupVC {
+    //MARK: - Custom Methods
+}
