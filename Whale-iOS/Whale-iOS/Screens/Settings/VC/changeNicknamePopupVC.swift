8b999b6f28da3806519ddc2d790abbce308d58dd
Danna <daeunlee0720@gmail.com>
2021-10-01 17:43:05 +0900

    [feat] 닉네임 변경 뷰컨 생성

---
+++ Whale-iOS/Whale-iOS/Screens/Settings/VC/changeNicknamePopupVC.swift
@@ -0,0 +1,17 @@
+//
+//  changeNicknamePopupVC.swift
+//  Whale-iOS
+//
+//  Created by Danna Lee on 2021/10/01.
+//
+
+import UIKit
+
+class changeNicknamePopupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
