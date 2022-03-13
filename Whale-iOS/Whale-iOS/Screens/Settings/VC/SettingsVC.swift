92c63ffb2a8f7ea07408a19780136b00d1962276
Danna <daeunlee0720@gmail.com>
2022-03-13 18:23:01 +0900

    feat: 푸시알림 구현

--- Whale-iOS/Whale-iOS/Screens/Settings/VC/SettingsVC.swift
+++ Whale-iOS/Whale-iOS/Screens/Settings/VC/SettingsVC.swift
@@ -7,9 +7,12 @@
 
 import UIKit
 import MessageUI
+import UserNotifications
 
 class SettingsVC: UIViewController {
-
+    
+    let userNotificationCenter = UNUserNotificationCenter.current()
+    
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var titleUnderLineView: UIView!
     @IBOutlet var settingView: [UIView]!
@@ -23,6 +26,8 @@ class SettingsVC: UIViewController {
         super.viewDidLoad()
         
         setStyle()
+        
+        userNotificationCenter.delegate = self
         
         NotificationCenter.default.addObserver(self, selector: #selector(nicknameChanged(_:)), name: NSNotification.Name("nicknameChanged"), object: nil)
     }
@@ -44,7 +49,12 @@ class SettingsVC: UIViewController {
     
     @IBAction func changeAlertStateSwitch(_ sender: Any) {
         if changeAlertStateSwitch.isOn {
+            UserDefaults.standard.setValue(true, forKey: "isAlertOn")
             self.showToast(message: "앞으로 오전 9:00 에 칭찬 알림을 보내드릴게요!", bottom: 115)
+            self.requestAuthorization()
+        } else {
+            UserDefaults.standard.setValue(false, forKey: "isAlertOn")
+            UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: ["whaleTimeNoti"])
         }
     }
     
@@ -53,12 +63,12 @@ class SettingsVC: UIViewController {
             
             let composeVC = MFMailComposeViewController()
             composeVC.mailComposeDelegate = self
-
+            
             // Configure the fields of the interface.
             composeVC.setToRecipients(["Co.Goraedan@gmail.com"])
             composeVC.setSubject("[칭찬할고래 문의]")
             composeVC.setMessageBody("1. 문의 유형 ( 문의, 버그 제보, 탈퇴하기, 기타) : \n2. 회원 닉네임 (필요시 기입) : \n3. 문의 내용 : \n\n\n문의하신 사항은 칭찬할고래팀이 신속하게 처리하겠습니다. 감사합니다 :)", isHTML: false)
-
+            
             // Present the view controller modally.
             present(composeVC, animated: true) {
                 composeVC.showToast(message: "문의 내용을 적어 메일로 보내주세요!", bottom: 115)
@@ -108,6 +118,44 @@ extension SettingsVC {
         }
         
         settingDetailLabel[0].text = UserDefaults.standard.string(forKey: "nickName")
+        
+        if UserDefaults.standard.bool(forKey: "isAlertOn") {
+            print("on")
+            changeAlertStateSwitch.setOn(true, animated: false)
+        } else {
+            print("off")
+            changeAlertStateSwitch.setOn(false, animated: false)
+        }
+    }
+    
+    func requestAuthorization() {
+        let options = UNAuthorizationOptions(arrayLiteral: .alert, .badge, .sound)
+        userNotificationCenter.requestAuthorization(options: options) { success, error in
+            if let error = error {
+                print(error)
+            } else {
+                self.sendNotification()
+            }
+        }
+    }
+    
+    func sendNotification() {
+        let notiContent = UNMutableNotificationContent()
+        notiContent.title = "오늘의 칭찬이 도착했어요!"
+        notiContent.body = "지금 바로 오늘의 칭찬을 확인하고, 실천해보세요!"
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 9
+        dateComponents.minute = 0
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let request = UNNotificationRequest(identifier: "whaleTimeNoti", content: notiContent, trigger: trigger)
+        
+        userNotificationCenter.add(request) { error in
+            if let error = error {
+                print(error)
+            }
+        }
     }
 }
 
@@ -130,5 +178,15 @@ extension SettingsVC: MFMailComposeViewControllerDelegate {
             self.showToast(message: "메일 발송에 실패했어요 :(", bottom: 115)
         }
         dismiss(animated: true)
+    }
+}
+
+extension SettingsVC: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .badge, .sound, .banner])
     }
 }
